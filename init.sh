dcbf0ed08aabf58ec9127d03d2cff1e4326e38df
Eric D. Schabell <eric@schabell.org>
2016-02-01 10:31:44 -0500

    Fixed missing override patch flags.

--- init.sh
+++ init.sh
@@ -94,7 +94,7 @@ fi
 echo
 echo "Applying JBoss EAP 6.4.4 patch now..."
 echo
-$JBOSS_HOME/bin/jboss-cli.sh --command="patch apply $SRC_DIR/$EAP_PATCH --override-modules"
+$JBOSS_HOME/bin/jboss-cli.sh --command="patch apply $SRC_DIR/$EAP_PATCH --override-all"
 
 if [ $? -ne 0 ]; then
 	echo
